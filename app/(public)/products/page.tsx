7fe624678eb4ed8c41732179f685c53e3493a682
Triathlon-staf <serverbersama94@gmail.com>
2026-06-01 18:06:09 +0800

    Update page.tsx

--- app/(public)/products/page.tsx
+++ app/(public)/products/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import { Search, Filter } from "lucide-react";
 
+export const dynamic = 'force-dynamic';
+
 export default async function ProductsPage({
   searchParams,
 }: {
