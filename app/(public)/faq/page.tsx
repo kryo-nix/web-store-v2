6d100f843c56b916272fdb15fcfeb084e1a2b895
Triathlon-staf <serverbersama94@gmail.com>
2026-06-01 18:07:05 +0800

    Update page.tsx

--- app/(public)/faq/page.tsx
+++ app/(public)/faq/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 export default async function FAQPage() {
   // Pakai (prisma as any).fAQ
   const faqs = await (prisma as any).fAQ.findMany({
