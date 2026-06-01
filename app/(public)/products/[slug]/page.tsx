1cca554575c3d285ec6860885c55c6336a1c3124
Triathlon-staf <serverbersama94@gmail.com>
2026-06-01 18:05:41 +0800

    Update page.tsx

--- app/(public)/products/[slug]/page.tsx
+++ app/(public)/products/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 import { CheckCircle, ArrowLeft, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+
 export default async function ProductDetailPage({
   params,
 }: {
